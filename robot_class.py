4ba20136068c0560a33774075d30f064fdb26a4a
root <tobias.drees@gmail.com>
2020-01-22 11:40:40 +0000

    Edited sense function and completed notebook 1

--- robot_class.py
+++ robot_class.py
@@ -97,7 +97,7 @@ class robot:
         ## 3. If either of the distances, dx or dy, fall outside of the internal var, measurement_range
         ##    then we cannot record them; if they do fall in the range, then add them to the measurements list
         ##    as list.append([index, dx, dy]), this format is important for data creation done later
-            if abs(self.x) <= self.measurement_range and abs(self.y) <= self.measurement_range:
+            if abs(dx) <= self.measurement_range and abs(dy) <= self.measurement_range:
                 measurements.append([j, dx, dy])
         
         ## TODO: return the final, complete list of measurements
